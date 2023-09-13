405e1090775059aaab40e090bd856814ed573abb
ChristophMetzner <christoph.metzner@gmail.com>
2023-09-13 10:07:51 +0200

    Reduce IC->Thal E weight to 05 and Thal->E 1.1 and Thal->I 0.9

--- cfg.py
+++ cfg.py
@@ -68,7 +68,7 @@ cfg.recordDipole = True
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'control_ASSR_Poulami_IC200_weightE_05_Thal_EI_1_1'
+cfg.simLabel = 'control_ASSR_Poulami_IC200_weightE_05_Thal_E_1_1_Thal_I_0_9'
 cfg.saveFolder = 'data/ASSR_test'                	## Set file output name
 cfg.savePickle = True         	## Save pkl file
 cfg.saveJson = False           	## Save json file
