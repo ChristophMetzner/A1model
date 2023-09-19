edb76f7296feaa3355cfc5338995b4427fd28992
ChristophMetzner <christoph.metzner@gmail.com>
2023-09-19 10:16:44 +0200

    default+weightE=1.15+weightI=1.15+Thal-ITS4-ITP4-PV4=*1.5

--- netParams.py
+++ netParams.py
@@ -388,7 +388,7 @@ if cfg.addConn and cfg.addIntraThalamicConn:
                 elif post in TEpops:  # I->E
                     syn = SOMESynMech
                     synWeightFactor = cfg.synWeightFractionIE 
-                    scaleFactor = 1.5
+                    scaleFactor = 1.0 # 1.5
                 else:                  # I->I
                     syn = SOMISynMech
                     synWeightFactor = [1.0]
@@ -434,7 +434,7 @@ if cfg.addConn and cfg.addThalamoCorticalConn:
                     if post=='PV4':
                         syn = ESynMech
                         synWeightFactor = cfg.synWeightFractionEE
-                        scaleFactor = 1.0
+                        scaleFactor = 1.5
                     elif post=='SOM4':
                         syn = ESynMech
                         synWeightFactor = cfg.synWeightFractionEE
@@ -442,11 +442,11 @@ if cfg.addConn and cfg.addThalamoCorticalConn:
                     elif post=='ITS4':
                         syn = ESynMech
                         synWeightFactor = cfg.synWeightFractionEE
-                        scaleFactor = 1.0
+                        scaleFactor = 1.5
                     elif post=='ITP4':
                         syn = ESynMech
                         synWeightFactor = cfg.synWeightFractionEE
-                        scaleFactor = 1.0
+                        scaleFactor = 1.5
                     else:
                         syn = ESynMech
                         synWeightFactor = cfg.synWeightFractionEE
