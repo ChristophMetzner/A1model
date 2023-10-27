0a37cc9fa0b24d54ba7698fd57c9b58810520713
ChristophMetzner <christoph.metzner@gmail.com>
2023-10-27 09:51:47 +0200

    Thal->L4 grid search set up

--- batch_tu_mpi_ASSR.py
+++ batch_tu_mpi_ASSR.py
@@ -136,6 +136,83 @@ def assr_batch(filename):
     return b
 
 
+
+def assr_batch_grid(filename):
+    params = specs.ODict()
+
+    if not filename:
+        filename = 'data/v34_batch25/trial_2142/trial_2142_cfg.json'
+
+    # from prev 
+    import json
+    with open(filename, 'rb') as f:
+        cfgLoad = json.load(f)['simConfig']
+    cfgLoad2 = cfgLoad
+
+    # #### SET weights#### 
+    params[('thalL4PV')] = [0.5, 0.75]
+    params[('thalL4SOM')] = [0.5, 0.75]
+    params[('thalL4E')] = [1.25, 1.5]
+
+    #### GROUPED PARAMS #### 
+    groupedParams = [] 
+
+    # --------------------------------------------------------
+    # initial config
+    initCfg = {} # set default options from prev sim
+    
+    initCfg['duration'] = 6000 #11500 
+    initCfg['printPopAvgRates'] = [1500, 3500]
+    initCfg['scaleDensity'] = 1.0 
+    initCfg['recordStep'] = 0.05
+
+    # SET SEEDS FOR CONN AND STIM 
+    initCfg[('seeds', 'conn')] = 0
+
+
+
+
+    ### OPTION TO RECORD EEG / DIPOLE ###
+    initCfg['recordDipole'] = True
+    initCfg['saveCellSecs'] = False
+    initCfg['saveCellConns'] = False
+    
+    # from prev - best of 50% cell density
+    updateParams = ['EEGain', 'EIGain', 'IEGain', 'IIGain',
+                    ('EICellTypeGain', 'PV'), ('EICellTypeGain', 'SOM'), ('EICellTypeGain', 'VIP'), ('EICellTypeGain', 'NGF'),
+                    ('IECellTypeGain', 'PV'), ('IECellTypeGain', 'SOM'), ('IECellTypeGain', 'VIP'), ('IECellTypeGain', 'NGF'),
+                    ('EILayerGain', '1'), ('IILayerGain', '1'),
+                    ('EELayerGain', '2'), ('EILayerGain', '2'),  ('IELayerGain', '2'), ('IILayerGain', '2'), 
+                    ('EELayerGain', '3'), ('EILayerGain', '3'), ('IELayerGain', '3'), ('IILayerGain', '3'), 
+                    ('EELayerGain', '4'), ('EILayerGain', '4'), ('IELayerGain', '4'), ('IILayerGain', '4'), 
+                    ('EELayerGain', '5A'), ('EILayerGain', '5A'), ('IELayerGain', '5A'), ('IILayerGain', '5A'), 
+                    ('EELayerGain', '5B'), ('EILayerGain', '5B'), ('IELayerGain', '5B'), ('IILayerGain', '5B'), 
+                    ('EELayerGain', '6'), ('EILayerGain', '6'), ('IELayerGain', '6'), ('IILayerGain', '6')] 
+
+    for p in updateParams:
+        if isinstance(p, tuple):
+            initCfg.update({p: cfgLoad[p[0]][p[1]]})
+        else:
+            initCfg.update({p: cfgLoad[p]})
+
+    # good thal params for 100% cell density 
+    updateParams2 = ['thalamoCorticalGain', 'intraThalamicGain', 'EbkgThalamicGain', 'IbkgThalamicGain', 'wmat']
+
+    for p in updateParams2:
+        if isinstance(p, tuple):
+            initCfg.update({p: cfgLoad2[p[0]][p[1]]})
+        else:
+            initCfg.update({p: cfgLoad2[p]})
+
+
+
+    b = Batch(params=params, netParamsFile='netParams_ASSR.py', cfgFile='cfg_ASSR.py', initCfg=initCfg, groupedParams=groupedParams)
+    b.method = 'grid'
+
+    return b
+
+
+
 # ----------------------------------------------------------------------------------------------
 # Run configurations
 # ----------------------------------------------------------------------------------------------
@@ -156,9 +233,10 @@ if __name__ == '__main__':
 
 
 
-    b = assr_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
+    #b = assr_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
+    b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'ASSR_opt'   
+    b.batchLabel = 'ASSR_grid'   
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'mpi_direct')
