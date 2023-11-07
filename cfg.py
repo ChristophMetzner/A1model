d4f755ce4dcff5f3c5aa001bc0a8038b8778a92f
ChristophMetzner <christoph.metzner@gmail.com>
2023-11-07 10:08:12 +0100

    increase Thal->L4E

--- cfg.py
+++ cfg.py
@@ -67,7 +67,7 @@ cfg.recordDipole = True
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'ASSR_7_11_23_1'
+cfg.simLabel = 'ASSR_7_11_23_2'
 cfg.saveFolder = 'data/' + cfg.simLabel  ## Set file output name
 cfg.savePickle = True         							## Save pkl file
 cfg.saveJson = False           							## Save json file
@@ -192,7 +192,7 @@ cfg.ICThalprobIMatrix = cfg.ICThalprobICore
 # these params added from Christoph Metzner branch
 cfg.thalL4PV = 0.5 
 cfg.thalL4SOM = 0.5 
-cfg.thalL4E = 1.0 
+cfg.thalL4E = 1.5 #1.0 
 
 cfg.addSubConn = 1
 
