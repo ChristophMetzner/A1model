a40920a0ed984be60fed169e6abbd5ac0c75e88e
JasminTU <planet.theeia@gmail.com>
2023-11-16 09:42:13 +0100

    set up sim for PV changes

--- batch_tu_mpi.py
+++ batch_tu_mpi.py
@@ -92,7 +92,7 @@ def spont_batch(filename, seed):
 def setRunCfg(b, type='mpi_direct'):
     if type=='mpi_direct':
         b.runCfg = {'type': 'mpi_direct',
-            'cores': 20,
+            'cores': 15,
             'coresPerNode': 4,
             'script': 'init.py',
             'mpiCommand': 'mpiexec',
