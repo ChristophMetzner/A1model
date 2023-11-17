4783e164aeb053d565b21fd671ac09db3084a53d
JasminTU <planet.theeia@gmail.com>
2023-11-17 12:54:49 +0100

    test

--- batch_tu_mpi.py
+++ batch_tu_mpi.py
@@ -13,7 +13,7 @@ import numpy as np
 # ----------------------------------------------------------------------------------------------
 # Spontaneous activity different seeds
 # ----------------------------------------------------------------------------------------------
-def spont_batch(filename, seed):
+def spont_batch(filename):
     params = specs.ODict()
 
     if not filename:
@@ -41,7 +41,7 @@ def spont_batch(filename, seed):
     initCfg['recordStep'] = 0.05
 
     # SET SEEDS FOR CONN AND STIM 
-    initCfg[('seeds', 'conn')] = seed
+    initCfg[('seeds', 'conn')] = [0, 1, 2, 3]
     initCfg[('seeds', 'stim')] = 0
 
 
@@ -92,7 +92,7 @@ def spont_batch(filename, seed):
 def setRunCfg(b, type='mpi_direct'):
     if type=='mpi_direct':
         b.runCfg = {'type': 'mpi_direct',
-            'cores': 8,
+            'cores': 2,
             'coresPerNode': 4,
             'script': 'init.py',
             'mpiCommand': 'mpiexec',
@@ -104,24 +104,24 @@ def setRunCfg(b, type='mpi_direct'):
 
 if __name__ == '__main__':
 
-    seeds = [7, 8, 9]
-
-    for seed in seeds:
-        b = spont_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json', seed)
-        b.batchLabel = f'v35_PV_20000ms_seed_{seed}'
-        b.saveFolder = 'data/' + b.batchLabel
-        setRunCfg(b, 'mpi_direct')  # 'hpc_slurm_gcp') #'mpi_bulletin') #'hpc_slurm_gcp')
-        b.run()  # run batch
-
-
-
-    # b = spont_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
+    # seeds = [7, 8, 9]
     #
-    # b.batchLabel = 'v35_PV_20000ms'
-    # b.saveFolder = 'data/'+b.batchLabel
-    #
-    # setRunCfg(b, 'mpi_direct')
-    # b.run() # run batch
+    # for seed in seeds:
+    #     b = spont_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json', seed)
+    #     b.batchLabel = f'v35_PV_20000ms_seed_{seed}'
+    #     b.saveFolder = 'data/' + b.batchLabel
+    #     setRunCfg(b, 'mpi_direct')  # 'hpc_slurm_gcp') #'mpi_bulletin') #'hpc_slurm_gcp')
+    #     b.run()  # run batch
+
+
+
+    b = spont_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
+
+    b.batchLabel = 'v35_PV_20000ms_test'
+    b.saveFolder = 'data/'+b.batchLabel
+
+    setRunCfg(b, 'mpi_direct')
+    b.run() # run batch
 
 
 
