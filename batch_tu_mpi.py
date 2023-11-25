4b2f818a7ba17d01fb13a74bcee83e8e9a6a5db2
JasminTU <planet.theeia@gmail.com>
2023-11-25 10:48:11 +0100

    second batch of seeds for PV changes

--- batch_tu_mpi.py
+++ batch_tu_mpi.py
@@ -26,7 +26,7 @@ def spont_batch(filename):
     cfgLoad2 = cfgLoad
 
     # #### SET CONN AND STIM SEEDS #### 
-    params[('seeds', 'conn')] = [1, 2]
+    params[('seeds', 'conn')] = [3, 5]
 
 
 
@@ -93,7 +93,7 @@ def spont_batch(filename):
 def setRunCfg(b, type='mpi_direct'):
     if type=='mpi_direct':
         b.runCfg = {'type': 'mpi_direct',
-            'cores': 16,
+            'cores': 32,
             'script': 'init.py',
             'mpiCommand': 'mpiexec',
             'skip': True}
@@ -117,7 +117,7 @@ if __name__ == '__main__':
 
     b = spont_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'v35_PV_20000ms_remaining_seeds_1'
+    b.batchLabel = 'v35_PV_20000ms_remaining_seeds_2'
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'mpi_direct')
