93222dfe1808056ed9855d6eed4311b81867804c
JasminTU <planet.theeia@gmail.com>
2023-12-01 17:16:01 +0100

    fifth batch of seeds for PV changes

--- batch_tu_mpi.py
+++ batch_tu_mpi.py
@@ -93,7 +93,7 @@ def spont_batch(filename):
 def setRunCfg(b, type='mpi_direct'):
     if type=='mpi_direct':
         b.runCfg = {'type': 'mpi_direct',
-            'cores': 32,
+            'cores': 16,
             'script': 'init.py',
             'mpiCommand': 'mpiexec',
             'skip': True}
