0730eea0f52718a43700ed4988e351c12fbed53c
ChristophMetzner <christoph.metzner@gmail.com>
2023-12-04 14:21:44 +0100

    4_12_23_1

--- cfg.py
+++ cfg.py
@@ -67,7 +67,7 @@ cfg.recordDipole = True
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'ASSR_20_11_23_3'
+cfg.simLabel = 'ASSR_4_12_23_1'
 cfg.saveFolder = 'data/' + cfg.simLabel  ## Set file output name
 cfg.savePickle = True         							## Save pkl file
 cfg.saveJson = False           							## Save json file
@@ -175,10 +175,11 @@ cfg.intraThalamicGain = 1.0
 cfg.corticoThalamicGain = 1.0
 
 # these params control IC -> Thalamic Core
-cfg.ICThalweightECore = 2.5 # 1.0
+cfg.ICThalweightECore = 1.83 # 1.0
 cfg.ICThalweightICore = 0.75 # 0.25
 cfg.ICThalprobECore = 0.16
 cfg.ICThalprobICore = 0.09
+
 # these params control IC -> Thalamic Matrix
 cfg.ICThalMatrixCoreFactor = 0.12 #0.1
 cfg.ICThalweightEMatrix = cfg.ICThalweightECore * cfg.ICThalMatrixCoreFactor
@@ -189,8 +190,7 @@ cfg.ICThalprobIMatrix = cfg.ICThalprobICore
 # these params added from Christoph Metzner branch
 cfg.thalL4PV = 0.075 # 0.5 
 cfg.thalL4SOM = 0.25 # 0.5 
-cfg.thalL4E = 4.0 #1.0 
-
+cfg.thalL4E = 2.0 #1.0 
 
 
 cfg.addSubConn = 1
