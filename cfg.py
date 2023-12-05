902648efa8003a691b2d9353e65705cbaf1831d1
ChristophMetzner <christoph.metzner@gmail.com>
2023-12-05 09:10:22 +0100

    5_12_23_2

--- cfg.py
+++ cfg.py
@@ -67,7 +67,7 @@ cfg.recordDipole = True
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'ASSR_5_12_23_1'
+cfg.simLabel = 'ASSR_5_12_23_2'
 cfg.saveFolder = 'data/' + cfg.simLabel  ## Set file output name
 cfg.savePickle = True         							## Save pkl file
 cfg.saveJson = False           							## Save json file
@@ -189,7 +189,7 @@ cfg.ICThalprobIMatrix = cfg.ICThalprobICore
 # these params added from Christoph Metzner branch
 cfg.thalL4PV = 0.075 # 0.5 
 cfg.thalL4SOM = 0.25 # 0.5 
-cfg.thalL4E = 3.0 #1.0 
+cfg.thalL4E = 2.5 #1.0 
 
 
 cfg.addSubConn = 1
