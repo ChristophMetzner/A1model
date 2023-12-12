bf6320ba13eeb78abff85eee757ece93d621fc74
ChristophMetzner <christoph.metzner@gmail.com>
2023-12-12 11:02:14 +0100

    12_12_23_4

--- cfg.py
+++ cfg.py
@@ -67,7 +67,7 @@ cfg.recordDipole = True
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'ASSR_12_12_23_3'
+cfg.simLabel = 'ASSR_12_12_23_4'
 cfg.saveFolder = 'data/' + cfg.simLabel  ## Set file output name
 cfg.savePickle = True         							## Save pkl file
 cfg.saveJson = False           							## Save json file
@@ -191,9 +191,9 @@ cfg.thalL4PV = 0.075 # 0.5
 cfg.thalL4SOM = 0.25 # 0.5 
 cfg.thalL4E = 2.5 #1.0 
 
-cfg.L4L3E    = 1.0 # 1.0
-cfg.L4L3PV   = 1.0
-cfg.L4L3SOM  = 1.0
+cfg.L4L3E    = 1.5 # 1.0
+cfg.L4L3PV   = 0.5
+cfg.L4L3SOM  = 1.0 
 
 
 cfg.addSubConn = 1
