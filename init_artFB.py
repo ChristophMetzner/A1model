76ed586dcd430ab164b034e30d2081a61856d6b7
ChristophMetzner <christoph.metzner@gmail.com>
2024-01-29 10:43:34 +0100

    feedback inputs and altered syanptic placement code

--- init_artFB.py
+++ init_artFB.py
@@ -19,7 +19,7 @@ import matplotlib; matplotlib.use('Agg')  # to avoid graphics error in servers
 
 from netpyne import sim
 
-cfg, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
+cfg, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams_addFB.py')
 
 # sim.createSimulateAnalyze(netParams, cfg)
 print(cfg.simLabel)
