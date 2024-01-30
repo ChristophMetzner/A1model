cff32201cfc59fec2806eb733a985afa03f6590f
ChristophMetzner <christoph.metzner@gmail.com>
2024-01-30 14:48:37 +0100

    30_01_24_alterSyn_test_4

--- cfg.py
+++ cfg.py
@@ -67,7 +67,7 @@ cfg.recordDipole = True
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'ASSR_30_01_24_test_seed'
+cfg.simLabel = 'ASSR_30_01_24_artFB_test_4'
 cfg.saveFolder = 'data/' + cfg.simLabel  ## Set file output name
 cfg.savePickle = True         							## Save pkl file
 cfg.saveJson = False           							## Save json file
@@ -219,8 +219,8 @@ cfg.L4L3SOM  = 1.0
 cfg.L4L3VIP = 0.1
 cfg.L4L3NGF = 1.0
 
-cfg.artFB = 0
-cfg.artFBweight = 5.0
+cfg.artFB = 1
+cfg.artFBweight = 10.0
 cfg.artFBprob = 0.5
 
 cfg.addSubConn = 1
