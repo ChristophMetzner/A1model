f844c948e00f9bf62465873b59186c35da2bfc91
ChristophMetzner <christoph.metzner@gmail.com>
2024-01-31 15:15:42 +0100

    31_01_24_artFB_test1

--- cfg.py
+++ cfg.py
@@ -67,7 +67,7 @@ cfg.recordDipole = True
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'ASSR_31_01_24_alterSyn_test_1'
+cfg.simLabel = 'ASSR_31_01_24_artFB_test_1'
 cfg.saveFolder = 'data/' + cfg.simLabel  ## Set file output name
 cfg.savePickle = True         							## Save pkl file
 cfg.saveJson = False           							## Save json file
@@ -219,7 +219,7 @@ cfg.L4L3SOM  = 1.0
 cfg.L4L3VIP = 0.1
 cfg.L4L3NGF = 1.0
 
-cfg.artFB = 0
+cfg.artFB = 1
 cfg.artFBweight = 10.0
 cfg.artFBprob = 0.5
 
@@ -230,7 +230,7 @@ cfg.addSubConn = 1
 with open('conn/conn.pkl', 'rb') as fileObj: connData = pickle.load(fileObj)
 cfg.wmat = connData['wmat']
 
-cfg.seeds = {'conn': 12345, 'stim': 1, 'loc': 1}
+cfg.seeds = {'conn': 23451, 'stim': 1, 'loc': 1}
 #------------------------------------------------------------------------------
 # Background inputs
 #------------------------------------------------------------------------------
