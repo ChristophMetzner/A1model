51855294881b7a0e77f732b10ce65b0c353adfd9
ChristophMetzner <christoph.metzner@gmail.com>
2024-02-02 09:52:11 +0100

    02_02_24_alterSyn_test1

--- netParams_addFB_alterSyn.py
+++ netParams_addFB_alterSyn.py
@@ -668,30 +668,30 @@ if cfg.addSubConn:
 
     #------------------------------------------------------------------------------
     #  TCM -> E: apical
+    netParams.subConnParams['TCM->E'] = {
+        'preConds': {'cellType': ['TCM']}, 
+        'postConds': {'cellType': ['IT', 'ITS4', 'PT', 'CT']},
+        'sec': 'apic_trunk',#'apic',
+        'groupSynMechs': ESynMech, 
+        'density': 'uniform'}
+
+    #------------------------------------------------------------------------------
+    #  TCM -> E: apical
     #netParams.subConnParams['TCM->E'] = {
     #    'preConds': {'cellType': ['TCM']}, 
-    #    'postConds': {'cellType': ['IT', 'ITS4', 'PT', 'CT']},
+    #    'postConds': {'cellType': ['IT2','IT3','IT6', 'ITS4', 'CT']},
     #    'sec': 'apic',
     #    'groupSynMechs': ESynMech, 
     #    'density': 'uniform'}
 
     #------------------------------------------------------------------------------
-    #  TCM -> E: apical
-    netParams.subConnParams['TCM->E'] = {
-        'preConds': {'cellType': ['TCM']}, 
-        'postConds': {'cellType': ['IT2','IT3','IT6', 'ITS4', 'CT']},
-        'sec': 'apic',
-        'groupSynMechs': ESynMech, 
-        'density': 'uniform'}
-
-    #------------------------------------------------------------------------------
     #  TCM -> 5: apical
-    netParams.subConnParams['TCM->5'] = {
-        'preConds': {'cellType': ['TCM']}, 
-        'postConds': {'cellType': ['IT5A','IT5B','PT5B']},
-        'sec': 'apic_trunk',
-        'groupSynMechs': ESynMech, 
-        'density': 'uniform'}
+    #netParams.subConnParams['TCM->5'] = {
+    #    'preConds': {'cellType': ['TCM']}, 
+    #    'postConds': {'cellType': ['IT5A','IT5B','PT5B']},
+    #    'sec': 'apic_trunk',
+    #    'groupSynMechs': ESynMech, 
+    #    'density': 'uniform'}
         
 
 #------------------------------------------------------------------------------
