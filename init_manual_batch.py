931c3511f27dbd9d65d470e17567c380f1ac2342
ChristophMetzner <christoph.metzner@gmail.com>
2024-02-06 10:21:52 +0100

    manual batches scripts

---
+++ init_manual_batch.py
@@ -0,0 +1,44 @@
+"""
+init.py
+
+Starting script to run multi-seed batch of the NetPyNE-based A1 model.
+
+
+
+Contributors: ericaygriffith@gmail.com, salvadordura@gmail.com, christoph.metzner@gmail.com
+"""
+
+import matplotlib; matplotlib.use('Agg')  # to avoid graphics error in servers
+
+from netpyne import sim
+
+
+
+def run(seed,label)
+	cfg, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams_addFV_alterSyn.py')
+
+	cfg.seeds = {'conn': seed, 'stim': 1, 'loc': 1}
+	cfg.simLabel = 'label'
+	# sim.createSimulateAnalyze(netParams, cfg)
+	print(cfg.simLabel)
+	sim.initialize(simConfig = cfg, netParams = netParams)  				# create network object and set cfg and net params
+	sim.net.createPops()               			# instantiate network populations
+	sim.net.createCells()              			# instantiate network cells based on defined populations
+	sim.net.connectCells()            			# create connections between cells based on params
+	sim.net.addStims() 							# add network stimulation
+	sim.setupRecording()              			# setup variables to record for each cell (spikes, V traces, etc)
+	sim.runSim()                      			# run parallel Neuron simulation  
+
+	# distributed saving (to avoid errors with large output data)
+	sim.saveDataInNodes()
+	sim.gatherDataFromFiles()
+	
+	sim.saveData()  
+
+def main():
+	args = sys.argv[1:]
+	
+	run(args[0],args[1])
+	
+if __name__ == '__main__':
+	main()
---
+++ run_manual_batch.py
@@ -0,0 +1,9 @@
+import subprocess as sp
+
+seeds = [12345, 23451]#, 34512, 45123, 51234]
+
+for seed in seeds:
+	label = 'test'+str(seed) 
+	call  = 'mpiexec -n 32 nrniv -python -mpi init_manual_batch.py '+str(seed)+' '+label
+	print call
+	sp.call([call],shell=True)
