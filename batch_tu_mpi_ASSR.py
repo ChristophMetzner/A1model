c0b1ec45ec6745a2ddf508c98933ca117b3d4527
ChristophMetzner <christoph.metzner@gmail.com>
2024-02-06 11:01:34 +0100

    update ASSR batch scripts

--- batch_tu_mpi_ASSR.py
+++ batch_tu_mpi_ASSR.py
@@ -242,7 +242,7 @@ if __name__ == '__main__':
     #b = assr_batch('data/v34_batch25/trial_2142/trial_2142_cfg.json')
     b = assr_batch_grid('data/v34_batch25/trial_2142/trial_2142_cfg.json')
 
-    b.batchLabel = 'ASSR_noise_grid_15_01_24_part2'   
+    b.batchLabel = 'ASSR_artFBweight_06_02_24'   
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'mpi_direct')
